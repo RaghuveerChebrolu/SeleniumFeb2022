b3a8cc729368b567216e4f32e981ac98cde7ee49
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2022-02-26 10:12:19 +0530

    26Feb2022 Selenium xpath and reusable functions

--- src/test/java/com/testNg/TestNg1.java
+++ src/test/java/com/testNg/TestNg1.java
@@ -7,6 +7,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,21 +22,21 @@ import org.testng.annotations.AfterSuite;
 
 public class TestNg1 {
 	WebDriver driver;
-	static String qty = "5";
+	String qty = "5";
 
 	@Test(priority = 0)
 	public void ValidateGmoOnlineLoadedSuccessfully() {
 		String title = driver.getTitle();
 		// below are the 
-		// System.out.println(title);
-		// Assert.assertEquals(title, "Welcome to Green Mountain Outpos");
+		 System.out.println(title);
+		Assert.assertEquals(title, "Welcome to Green Mountain Outpost");
 	}
 
 	@Test(priority = 1, dependsOnMethods = { "ValidateGmoOnlineLoadedSuccessfully" })
 	public void ValidatioEnterGmoOnline() {
 		driver.findElement(By.name("bSubmit")).click();
-		driver.findElement(By.name("QTY_TENTS")).clear();
-		driver.findElement(By.name("QTY_TENTS")).sendKeys(qty);
+		Order3PersonDomeTent();
+		OrderExternalFrameBackpack();
 		driver.findElement(By.name("bSubmit")).click();
 		String unitPrice = driver
 				.findElement(By.cssSelector(
@@ -41,6 +44,21 @@ public class TestNg1 {
 				.getText();
 		System.out.println(unitPrice);
 
+	}
+
+	 void Order3PersonDomeTent() {
+		driver.findElement(By.name("QTY_TENTS")).clear();
+		driver.findElement(By.name("QTY_TENTS")).sendKeys(qty);
+	}
+	
+	public void OrderExternalFrameBackpack() {
+		driver.findElement(By.name("QTY_BACKPACKS")).clear();
+		driver.findElement(By.name("QTY_BACKPACKS")).sendKeys(qty);
+	}
+	
+	public void OrderGlacierSunGlasses() {
+		driver.findElement(By.xpath("//input[@type='text' and @name='QTY_GLASSES']")).clear();
+		driver.findElement(By.xpath("//input[@type='text' and @name='QTY_GLASSES']")).sendKeys(qty);
 	}
 
 	@Test(priority=2)
@@ -54,9 +72,9 @@ public class TestNg1 {
 		int UnitPricelength = UnitPrice.length();
 		System.out.println("UnitPrice:"+UnitPrice);
 		System.out.println("UnitPricelength:"+UnitPricelength);
-		String removeDollerFromUnixPrice = UnitPrice.substring(2);
-		System.out.println("removeDollerFromUnixPrice"+removeDollerFromUnixPrice);
-		Float calculatedPrice = Float.parseFloat(removeDollerFromUnixPrice)*Float.parseFloat(qty);
+		String removeDollerFromUnitPrice = UnitPrice.substring(2);
+		System.out.println("removeDollerFromUnixPrice"+removeDollerFromUnitPrice);
+		Float calculatedPrice = Float.parseFloat(removeDollerFromUnitPrice)*Float.parseFloat(qty);
 		System.out.println("calculatedPrice:"+calculatedPrice);
 		String TotalPriceFromWebpagewithDoller = driver.findElement(By.xpath("//table[@border=1]/tbody/tr[2]/td[5]")).getText();
 		System.out.println("TotalPriveFromWebpage:"+TotalPriceFromWebpagewithDoller);
@@ -64,30 +82,36 @@ public class TestNg1 {
 		System.out.println("ToatlPriceWebpage:"+ToatlPriceWebpage);
 		Float ToatlPriceWebpageinFLoat=Float.parseFloat(ToatlPriceWebpage);
 		Assert.assertEquals(calculatedPrice,ToatlPriceWebpageinFLoat);
-		
+		ValidatioEnterGmoOnline();
 	}
 	@BeforeMethod
 	public void beforeMethod() {
+		System.out.println("inside beforeMethod");
 	}
 
 	@AfterMethod
 	public void afterMethod() {
+		System.out.println("inside afterMethod");
 	}
 
 	@BeforeClass
 	public void beforeClass() {
+		System.out.println("inside beforeClass");
 	}
 
 	@AfterClass
 	public void afterClass() {
+		System.out.println("inside afterClass");
 	}
 
 	@BeforeTest
 	public void beforeTest() {
+		System.out.println("inside beforeTest");
 	}
 
 	@AfterTest
 	public void afterTest() {
+		System.out.println("inside afterTest");
 	}
 
 	@BeforeSuite
@@ -97,6 +121,12 @@ public class TestNg1 {
 		driver = new ChromeDriver();
 		driver.get("http://demo.borland.com/gmopost/");
 		driver.manage().window().maximize();
+		/*
+		 * implicit Wait : Global waiting mechanism applicable for all web Elements
+		 * declared under implicit wait. script is going to wait until web element is
+		 * Recognized upto maximum of 60 seconds. If not recognised it will throw noSuchElement Exception Timeout Exception 
+		 */
+		driver.manage().timeouts().implicitlyWait(60,TimeUnit.SECONDS);
 	}
 
 	@AfterSuite
