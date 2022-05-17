b20c4c3cb27f3fddbe76470ac2bdb5322c2188af
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2022-05-17 13:13:17 +0530

    17 May 2022 Java OOPS and POM.xml changes

---
+++ src/test/java/com/oops/finalClass.java
@@ -0,0 +1,16 @@
+package com.oops;
+
+//if a class is declared as final we cannot extend or inherit it and we cannot perform overloading and overriding.
+   final class Bike3121 {
+	 void run(){
+		 System.out.println("bike is running");
+	 }
+}
+
+/*
+ * class finalClass extends Bike3121 { void run() {
+ * System.out.println("running safely with 100kmph"); }
+ * 
+ * public static void main(String args[]) { finalClass honda = new finalClass();
+ * honda.run(); } }
+ */
